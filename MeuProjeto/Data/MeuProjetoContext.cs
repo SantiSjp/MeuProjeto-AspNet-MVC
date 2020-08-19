d9198322fb5ec0151b9ac7c12f4d73d6fdcb4e40
santisjp <santisjp@live.com>
2020-08-19 13:00:17 -0300

    Create Departments MVC

---
+++ MeuProjeto/Data/MeuProjetoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MeuProjeto.Models;
+
+namespace MeuProjeto.Data
+{
+    public class MeuProjetoContext : DbContext
+    {
+        public MeuProjetoContext (DbContextOptions<MeuProjetoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MeuProjeto.Models.Department> Department { get; set; }
+    }
+}
