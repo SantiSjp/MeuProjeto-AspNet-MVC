06ec85404da459c89f288529ab985ef1040f225f
santisjp <santisjp@live.com>
2020-08-19 13:14:17 -0300

    First Migration mysql

--- MeuProjeto/Startup.cs
+++ MeuProjeto/Startup.cs
@@ -37,7 +37,8 @@ namespace MeuProjeto
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<MeuProjetoContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MeuProjetoContext")));
+            options.UseMySql(Configuration.GetConnectionString("MeuProjetoContext"), builder =>
+            builder.MigrationsAssembly("MeuProjeto")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
