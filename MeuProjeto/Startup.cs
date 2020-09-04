632b86a299a21158c6dda4903138c6069e08702f
santisjp <santisjp@live.com>
2020-09-04 18:45:16 -0300

    Criando Seeding Service para popular banco

--- MeuProjeto/Startup.cs
+++ MeuProjeto/Startup.cs
@@ -39,14 +39,17 @@ namespace MeuProjeto
             services.AddDbContext<MeuProjetoContext>(options =>
             options.UseMySql(Configuration.GetConnectionString("MeuProjetoContext"), builder =>
             builder.MigrationsAssembly("MeuProjeto")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
