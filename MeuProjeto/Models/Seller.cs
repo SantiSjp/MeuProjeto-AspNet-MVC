c7343c22a8caaea9eb53d0cac4969a044d85ae48
santisjp <santisjp@live.com>
2020-09-05 17:01:45 -0300

    Definindo Locale e Formatando Data

--- MeuProjeto/Models/Seller.cs
+++ MeuProjeto/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace MeuProjeto.Models
@@ -9,8 +10,19 @@ namespace MeuProjeto.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        //Customizar Nome na View
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        //Customizar Nome na View
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         // Chave Estrangeira para Department
