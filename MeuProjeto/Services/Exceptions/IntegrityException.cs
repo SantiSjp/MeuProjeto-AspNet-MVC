aa157fc16296e118d91004cf83dde922942c3434
santisjp <santisjp@live.com>
2020-09-05 20:36:30 -0300

    Criado Exception Integrity para Delete

---
+++ MeuProjeto/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace MeuProjeto.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+
+        public IntegrityException(string message) : base()
+        {
+
+        }
+
+    }
+}
