9a5cce7cda2a9a28ec0af2da34501ae058cc44be
santisjp <santisjp@live.com>
2020-09-05 17:40:30 -0300

    Definindo operações Assíncronas de BD

--- MeuProjeto/Controllers/SellersController.cs
+++ MeuProjeto/Controllers/SellersController.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace MeuProjeto.Controllers
 {
@@ -22,18 +23,18 @@ namespace MeuProjeto.Controllers
             _departmentService = departmentService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
 
-            var list = _sellerService.FindAll();
+            var list = await _sellerService.FindAllAsync();
 
             return View(list);
         }
 
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
 
-            var departments = _departmentService.FindAll();
+            var departments =await  _departmentService.FindAllAsync();
             var viewModel = new SellerFormViewModel { Departments = departments };
 
             return View(viewModel);
@@ -43,12 +44,12 @@ namespace MeuProjeto.Controllers
         [HttpPost]
         // Previne ataque CSRF
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller)
+        public async Task<IActionResult> Create(Seller seller)
         {
             // Validação no lado do servidor
             if (!ModelState.IsValid)
             {
-                var departments = _departmentService.FindAll();
+                var departments = await _departmentService.FindAllAsync();
                 var viewModel = new SellerFormViewModel
                 {
                     Seller = seller,
@@ -56,19 +57,19 @@ namespace MeuProjeto.Controllers
                 };
                 return View(viewModel);
             }
-            _sellerService.Insert(seller);
+            await _sellerService.InsertAsync(seller);
             return RedirectToAction(nameof(Index));
         }
 
 
-        public IActionResult Delete(int? id)
+        public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "ID is NULL" });
             }
 
-            var obj = _sellerService.FindById(id.Value);
+            var obj = await _sellerService.FindByIdAsync(id.Value);
             if (obj == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "ID not found" });
@@ -81,21 +82,21 @@ namespace MeuProjeto.Controllers
         [HttpPost]
         // Previne ataque CSRF
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
             
-            _sellerService.Remove(id);
+            await _sellerService.RemoveAsync(id);
             return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Details(int? id)
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "ID is NULL" });
             }
 
-            var obj = _sellerService.FindById(id.Value);
+            var obj = await _sellerService.FindByIdAsync(id.Value);
             if (obj == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "ID not found" });
@@ -104,7 +105,7 @@ namespace MeuProjeto.Controllers
             return View(obj);
         }
 
-        public IActionResult Edit(int? id)
+        public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
@@ -112,13 +113,13 @@ namespace MeuProjeto.Controllers
             }
 
             // Id.value porque id é opcional "int?"
-            var obj = _sellerService.FindById(id.Value);
+            var obj = await _sellerService.FindByIdAsync(id.Value);
             if (obj == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "ID not found" });
             }
 
-            List<Department> departments = _departmentService.FindAll();
+            List<Department> departments = await _departmentService.FindAllAsync();
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj,Departments = departments };
 
             return View(viewModel);
@@ -128,12 +129,12 @@ namespace MeuProjeto.Controllers
         [HttpPost]
         // Previne ataque CSRF
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Seller seller)
+        public async Task<IActionResult> Edit(int id, Seller seller)
         {
 
             if (!ModelState.IsValid)
             {
-                var departments = _departmentService.FindAll();
+                var departments = await _departmentService.FindAllAsync();
                 var viewModel = new SellerFormViewModel
                 {
                     Seller = seller,
@@ -149,7 +150,7 @@ namespace MeuProjeto.Controllers
 
             try
             {
-                _sellerService.Update(seller);
+               await  _sellerService.UpdateAsync(seller);
             }
             catch (ApplicationException e)
             {
