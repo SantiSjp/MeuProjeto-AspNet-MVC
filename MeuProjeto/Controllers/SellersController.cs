092a2aa40d76aa251ae23eb70b9937a02c7ab3a7
santisjp <santisjp@live.com>
2020-09-05 13:16:54 -0300

    Criar Método Delete Seller

--- MeuProjeto/Controllers/SellersController.cs
+++ MeuProjeto/Controllers/SellersController.cs
@@ -44,5 +44,32 @@ namespace MeuProjeto.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // Informa método Post
+        [HttpPost]
+        // Previne ataque CSRF
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
